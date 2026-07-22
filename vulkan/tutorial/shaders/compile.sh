c496621b7c2c43a151d0784f9189de5d5d270c87
Landwhich <156349813+Landwhich@users.noreply.github.com>
2026-07-21 20:38:44 -0400

    done with the vulkan tutorial for now

--- vulkan/tutorial/shaders/compile.sh
+++
@@ -1,5 +0,0 @@
-export VXX=$HOME/VulkanSDK/1.4.341.1/macOS/bin/slangc
-
-export VXXFLAGS=("-target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain")
-
-$VXX shaders/shader.slang $VXXFLAGS -o slang.spv
